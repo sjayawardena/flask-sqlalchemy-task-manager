bb049f6c0b6cfe889d129cad659231efd094fc5f
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-02-13 11:43:59 +0000

    Add navbar from Materialize

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,8 @@
+
+// Initialise Materialize navbar
+
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initalisation
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, options);
+  });
