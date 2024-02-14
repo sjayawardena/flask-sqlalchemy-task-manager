30092c51a28ce4571306cc62c766a326f6309f19
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2024-02-14 18:59:32 +0000

    Add functionality to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,8 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
 
-// Initialise Materialize navbar
-
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initalisation
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
   });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+});
